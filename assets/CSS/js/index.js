e7d0ed57ea3502928ba9812845d04694a17259c5
alisha00754 <alishachudali352@gmail.com>
2025-06-04 14:59:12 +0545

    initial commit

---
+++ assets/CSS/js/index.js
@@ -0,0 +1,10 @@
+console.error("this is error")
+console.log("print this")
+console.timeEnd("where are you?")
+
+document.writeln("HELLO EVERYONE!!")
+document.getElementById("hello").innerHTML="how are you?"
+
+confirm("would you like to use this app")
+
+alert("Go to home page")
